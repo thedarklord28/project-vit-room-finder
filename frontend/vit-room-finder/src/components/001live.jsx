a2aae06b3427542b58e15b711873c2045b12b948
Advika Prasadh <advikaprasadh@gmail.com>
2026-07-16 17:32:48 +0530

    extract overall and blockwise rooms + slot timings map

--- frontend/vit-room-finder/src/components/001live.jsx
+++ frontend/vit-room-finder/src/components/001live.jsx
@@ -1,25 +1,66 @@
 import React, { useState, useEffect } from 'react'
-import classData from "../../rawdata/mockffcs.json" 
+import classData from "../../rawdata/mockffcs.json"
+import slotData from "../../rawdata/slotTimings.json"
 
 export default function Live() {
-    // Correctly targets the keys of the first item in the data array
-    const columns = classData && classData.length > 0 ? Object.keys(classData[0]) : [];
+    const [freeTheory, setFreeTheory] = useState(null);
+    const [freeLab, setFreeLab] = useState(null);
+    const [freeByBlock, setFreeByBlock]=useState({});
+    
+    
 
     useEffect(() => {
-        console.log("Extracted Columns:", columns);
-    }, [columns]);
+        const now = new Date();
+        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+        const currentDay = days[1];
+        const currentTimeStr = '08:30';
 
+        const todaysSchedule = slotData[currentDay] || { theory: [], lab: [] };
+        const currTheory = todaysSchedule.theory.find(t => currentTimeStr >= t.start && currentTimeStr <= t.end);
+        const currLab = todaysSchedule.lab.find(l => currentTimeStr >= l.start && currentTimeStr <= l.end);
+        const currTheorySlot = currTheory ? currTheory.slot : null;
+        const currLabSlot = currLab ? currLab.slot : null;
+
+        const freeRoomsSlots = classData.filter(item => {
+            const itemSlots = item.SLOT.split('+');
+            const isTheoryOccupied = currTheorySlot && itemSlots.includes(currTheorySlot);
+            const isLabOccupied = currLabSlot && itemSlots.includes(currLabSlot);
+
+            return !(isTheoryOccupied || isLabOccupied);
+
+        });
+
+        const freeTheorySlots = freeRoomsSlots.filter(item => {
+            return !(item.SLOT.includes('L'))
+        })
+
+        const freeLabSlots = freeRoomsSlots.filter(item => {
+            return (item.SLOT.includes('L'))
+        })
+
+        const freeTheoryRooms = [...new Set(freeTheorySlots.map(item => String(item.VENUE)||""))].sort()
+        const freeLabRooms = [...new Set(freeLabSlots.map(item => String(item.VENUE)||""))].sort()
+
+        setFreeTheory(freeTheoryRooms);
+        setFreeLab(freeLabRooms);
+
+        const blocks = ['AB1', 'AB2', 'AB3', 'AB4', 'AB5', 'ADB', 'MAB3', 'MAB4'];
+        const blocksMapping = {};
+
+        blocks.forEach(block=>{
+            blocksMapping[block]={
+                theory: freeTheoryRooms.filter(room=>room && room.includes(block)),
+                lab: freeLabRooms.filter(room=>room && room.includes(block))
+            }
+        },[])
+
+        setFreeByBlock(blocksMapping);
+
+        
+    })
     return (
-        <div className="w-screen h-screen bg-gray-500 p-8 flex flex-col gap-4 text-white">
-            <h1 className="text-2xl font-bold">Data Columns</h1>
-            
-            <ul className="list-disc pl-6 bg-gray-700 p-4 rounded shadow-md max-w-md">
-                {columns.map((colName, index) => (
-                    <li key={index} className="py-1 capitalize font-medium">
-                        {colName}
-                    </li>
-                ))}
-            </ul>
+        <div>
+            {console.log(freeByBlock['AB1'])}
         </div>
     )
 }
