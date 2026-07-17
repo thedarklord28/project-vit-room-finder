0fd11f25f97c1e66ad14713277ff801c124d3fa7
Advika Prasadh <advikaprasadh@gmail.com>
2026-07-17 06:11:08 +0530

    base model with display fo ab3 theory classrooms for testing

--- frontend/vit-room-finder/src/components/001live.jsx
+++ frontend/vit-room-finder/src/components/001live.jsx
@@ -5,62 +5,85 @@ import slotData from "../../rawdata/slotTimings.json"
 export default function Live() {
     const [freeTheory, setFreeTheory] = useState(null);
     const [freeLab, setFreeLab] = useState(null);
-    const [freeByBlock, setFreeByBlock]=useState({});
-    
-    
+    const [freeByBlock, setFreeByBlock] = useState({});
+    const [curDay, setCurDay] = useState('');
+    const [curTime, setCurTime] = useState('');
+    //test var rem later
+    const [ab3, setAb3]=useState(null);
+
 
     useEffect(() => {
         const now = new Date();
         const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-        const currentDay = days[1];
-        const currentTimeStr = '08:30';
+        const currentDay = days[now.getDay()];
+        const currentTimeStr = `${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}`;
+        //const currentTimeStr = "08:30";
+        if (currentDay)
+            setCurDay(currentDay);
+        if (currentTimeStr)
+            setCurTime(currentTimeStr);
 
-        const todaysSchedule = slotData[currentDay] || { theory: [], lab: [] };
-        const currTheory = todaysSchedule.theory.find(t => currentTimeStr >= t.start && currentTimeStr <= t.end);
-        const currLab = todaysSchedule.lab.find(l => currentTimeStr >= l.start && currentTimeStr <= l.end);
-        const currTheorySlot = currTheory ? currTheory.slot : null;
-        const currLabSlot = currLab ? currLab.slot : null;
+        if (currentDay && currentTimeStr) {
+            const todaysSchedule = slotData[currentDay] || { theory: [], lab: [] };
+            const currTheory = todaysSchedule.theory.find(t => currentTimeStr >= t.start && currentTimeStr <= t.end);
+            const currLab = todaysSchedule.lab.find(l => currentTimeStr >= l.start && currentTimeStr <= l.end);
+            const currTheorySlot = currTheory ? currTheory.slot : null;
+            const currLabSlot = currLab ? currLab.slot : null;
 
-        const freeRoomsSlots = classData.filter(item => {
-            const itemSlots = item.SLOT.split('+');
-            const isTheoryOccupied = currTheorySlot && itemSlots.includes(currTheorySlot);
-            const isLabOccupied = currLabSlot && itemSlots.includes(currLabSlot);
+            const freeRoomsSlots = classData.filter(item => {
+                const itemSlots = item.SLOT.split('+');
+                const isTheoryOccupied = currTheorySlot && itemSlots.includes(currTheorySlot);
+                const isLabOccupied = currLabSlot && itemSlots.includes(currLabSlot);
 
-            return !(isTheoryOccupied || isLabOccupied);
+                return !(isTheoryOccupied || isLabOccupied);
 
-        });
+            });
 
-        const freeTheorySlots = freeRoomsSlots.filter(item => {
-            return !(item.SLOT.includes('L'))
-        })
+            const freeTheorySlots = freeRoomsSlots.filter(item => {
+                return !(item.SLOT.includes('L'))
+            })
 
-        const freeLabSlots = freeRoomsSlots.filter(item => {
-            return (item.SLOT.includes('L'))
-        })
+            const freeLabSlots = freeRoomsSlots.filter(item => {
+                return (item.SLOT.includes('L'))
+            })
 
-        const freeTheoryRooms = [...new Set(freeTheorySlots.map(item => String(item.VENUE)||""))].sort()
-        const freeLabRooms = [...new Set(freeLabSlots.map(item => String(item.VENUE)||""))].sort()
+            const freeTheoryRooms = [...new Set(freeTheorySlots.map(item => String(item.VENUE) || ""))].sort()
+            const freeLabRooms = [...new Set(freeLabSlots.map(item => String(item.VENUE) || ""))].sort()
 
-        setFreeTheory(freeTheoryRooms);
-        setFreeLab(freeLabRooms);
+            setFreeTheory(freeTheoryRooms);
+            setFreeLab(freeLabRooms);
 
-        const blocks = ['AB1', 'AB2', 'AB3', 'AB4', 'AB5', 'ADB', 'MAB3', 'MAB4'];
-        const blocksMapping = {};
+            const blocks = ['AB1', 'AB2', 'AB3', 'AB4', 'AB5', 'ADB', 'MAB3', 'MAB4'];
+            const blocksMapping = {};
 
-        blocks.forEach(block=>{
-            blocksMapping[block]={
-                theory: freeTheoryRooms.filter(room=>room && room.includes(block)),
-                lab: freeLabRooms.filter(room=>room && room.includes(block))
-            }
-        },[])
+            blocks.forEach(block => {
+                blocksMapping[block] = {
+                    theory: freeTheoryRooms.filter(room => room && room.includes(block)),
+                    lab: freeLabRooms.filter(room => room && room.includes(block))
+                }
+            }, [])
 
-        setFreeByBlock(blocksMapping);
+            setFreeByBlock(blocksMapping);
+            setAb3(blocksMapping['AB3']);
+        }
 
-        
+
     })
+
     return (
-        <div>
-            {console.log(freeByBlock['AB1'])}
+        <div className='w-screen h-screen flex flex-col justify-between'>
+            <div className='w-full '>
+                <h1>Live Room View</h1>
+                <h1>{curDay}</h1>
+                <h1>{curTime}</h1>
+            </div>
+            <div>
+                {ab3 && ab3['theory'].map((room)=>{
+                    return(
+                        <p>{room}</p>
+                    )
+                })}
+            </div>
         </div>
     )
 }
