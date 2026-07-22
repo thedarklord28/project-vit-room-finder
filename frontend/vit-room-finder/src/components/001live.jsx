f0a5122b8762f7ea5199dabb5af12250c62a92b7
Advika Prasadh <advikaprasadh@gmail.com>
2026-07-22 23:14:21 +0530

    added toggle color

--- frontend/vit-room-finder/src/components/001live.jsx
+++ frontend/vit-room-finder/src/components/001live.jsx
@@ -130,9 +130,14 @@ export default function Live() {
                             onChange={(e) => setHideOccupied(e.target.checked)}
                             className="sr-only peer"
                         />
-                        <div className="relative w-9 h-5 bg-red-400 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-brand-soft dark:peer-focus:ring-brand-soft rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-buffer after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-brand"></div>
+                        <div className={`relative w-9 h-5 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-brand-soft dark:peer-focus:ring-brand-soft rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-buffer after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-brand 
+                            ${hideOccupied?
+                                        'bg-[#93C572]':
+                                        'bg-red-400'
+                                    }`}></div>
                         <span className="select-none ms-3 text-sm font-medium text-heading">Hide Occupied Classrooms</span>
                     </label>
+
                     {BLOCKS.map(block => {
                         const blockData = freeByBlock[block] || { theory: [], lab: [] };
                         const allBlockData = allByBlock[block] || { theory: [], lab: [] };
